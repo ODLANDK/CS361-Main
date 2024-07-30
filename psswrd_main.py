dcf20f439933b7304c8d2045ca2195c8dd522e28
odlandk <odlandk@oregonstate.edu>
2024-07-30 16:11:44 -0700

    7/30 - Added view and show password functionality 7/26 - Main program created, functionality for add, list, home, help added

--- psswrd_main.py
+++ psswrd_main.py
@@ -6,11 +6,12 @@
 import os
 import shelve
 
-entries = 0
 db = shelve.open("log", "c", writeback=True)
+
 
 def clear():
     os.system('clear' or 'cls')
+
 
 def home_screen():
     clear()
@@ -22,6 +23,7 @@ def home_screen():
     print("Choose how you'd like to get started:")
     commands()
 
+
 def commands():
 
     print("Type \"list\" to go to your list of passwords")
@@ -30,13 +32,19 @@ def commands():
     print("Type \"home\" any time to return to the home screen")
     print("Type \"exit\" any time to exit the program")
 
+
 def commands_additional():
     print("You can also interact with the list of password information")
     print("Here are some commands when looking at your password database: ")
     print("Type \"view [entry number]\" to view a specific entry")
-    print("Type \"view [website name]\" to view the entry for that website")
+    #print("Type \"view [website name]\" to view the entry for that website")
     print("Type \"delete [entry number]\" to delete a specific entry")
     print("Type \"delete [website name]\" to delete the entry for that website")
+
+    print("When viewing password information, here are some commands")
+    print("Type \"show\" to show the current password")
+    print("Type \"delete\" to delete the current password")
+
 
 def help_screen():
     clear()
@@ -48,6 +56,7 @@ def help_screen():
 
 
 def list_screen():
+
     clear()
     print("Password Information")
     print()
@@ -59,10 +68,10 @@ def list_screen():
         print(f"{db[keys]['entry']:<10}|     {db[keys]['site'].upper():<11}|"
               f"     {db[keys]['user']:<10}")
 
-    list_options()
+    print_list_options()
 
 
-def list_options():
+def print_list_options():
     print()
     print("Options:")
     print("add new entry")
@@ -71,8 +80,7 @@ def list_options():
     print("help")
 
 
-def add_screen():
-    global entries
+def add_screen(entry):
     clear()
     print("Add new password information")
     print("Enter 1 to return to list without saving")
@@ -84,8 +92,7 @@ def add_screen():
         user_name = input("add username: ")
         password = input("add password: ")
 
-        entries += 1
-        db[website] = {'entry': entries, 'site': website, 'user': user_name,
+        db[website] = {'entry': entry, 'site': website, 'user': user_name,
                        'password': password}
 
 
@@ -94,22 +101,59 @@ def clear_database():
 
 
 def view_screen():
-    pass
+
+    if user_input.lower() == "view":
+        view_entry = input("Which login information would you like to view? (enter #) ")
+        clear()
+    else:
+        view_entry = user_input.split(" ", 1)[1]
+
+    return view_information(view_entry)
 
 
-def show_screen():
-    pass
+def view_information(view_entry):
+
+    clear()
+    for keys in db.keys():
+        if db[keys]['entry'] == int(view_entry):
+            print("Website:          " + db[keys]['site'])
+            print("User Name:        " + db[keys]['user'])
+            print("Password:         ******")
+
+    print_view_options()
+    return keys
+
+
+def print_view_options():
+    print()
+    print("Options:")
+    print("show password")
+    print("delete password information")
+    print("back to list")
+    print("help")
+
+
+def show_screen(key):
+    clear()
+    print("Website:          " + db[key]['site'])
+    print("User Name:        " + db[key]['user'])
+    print("Password:         " + db[key]['password'])
+    print()
+    input("Press enter to return to view screen")
+    view_information(db[key]['entry'])
 
 
 def delete_screen():
+    # make sure to renumber entries after a delete. I think this would just be
+    # a loop over the keys
     print()
     print("WARNING: Any password information you delete is gone forever")
     del_choice = input("Are you sure you want to continue? (y/n) ")
     #if del_choice.lower() == "y":
 
 
-
-
+current_key = ''
+entries = len(db)
 home_screen()
 while True:
     print()
@@ -120,9 +164,15 @@ while True:
     elif user_input.lower() == "help":
         help_screen()
     elif user_input.lower() == "add":
-        add_screen()
+        entries += 1
+        add_screen(entries)
     elif user_input.lower() == "list":
         list_screen()
+        current_key = ''
+    elif user_input.lower()[:4] == "view":
+        current_key = view_screen()
+    elif user_input.lower() == "show":
+        show_screen(current_key)
     elif user_input.lower() == "clear":
         clear_database()
     elif user_input.lower() == "exit":
